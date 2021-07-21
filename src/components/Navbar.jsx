27c5b15530a0229ada68383264914c1bf5459de2
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-21 12:15:45 -0300

    Estiliza a navbar
    Estiliza e cria uma função para saber em qual parte do site o usuário está

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/navbar_s.css";
 
 function Navbar() {
-  return (
-    <nav>
-        
-     
-    </nav>
-  );
+	const url = useLocation();
+
+	useEffect(() => {
+		if (url.pathname === "/") {
+			document.getElementById("register").classList.add("active");
+		}
+
+		if (url.pathname === "/list") {
+			document.getElementById("list").classList.add("active");
+		}
+    
+	});
+
+	return (
+		<nav id="navbar">
+			<ul id="menu">
+				<li className="item_menu" id="register">
+					<Link to="/">Cadastrar</Link>
+				</li>
+				<li className="item_menu " id="list">
+					<Link to="/list">Listar</Link>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navbar;
