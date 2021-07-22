209355213bfb678dafbc3dd75f741976b6bccf69
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-22 15:12:33 -0300

    Lógica de remover o registro

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,45 +1,84 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import "../styles/mainList_s.css";
 
 function MainList() {
+	let [removed, setRemoved] = useState(false);
+
+	function remove(name) {
+		for (let index = 0; index < localStorage.length; index++) {
+			let list = JSON.parse(localStorage.getItem(index));
+			let new_data = {
+				name: "",
+				adress: "",
+				order: [],
+				active: true,
+			};
+
+			if (name === list.name) {
+				new_data.name = name;
+				new_data.active = false;
+				new_data.order.push({ flavor: "", quantity: 1, price: 10.0 });
+				localStorage.setItem(index, JSON.stringify(new_data));
+				toast.error(`${name} removido com sucesso!`, {
+					autoClose: 2500,
+					position: "top-center",
+					hideProgressBar: true,
+				});
+				setRemoved(true);
+			}
+		}
+	}
+
 	useEffect(function generateList() {
 		for (let index = 0; index < localStorage.length; index++) {
 			let data = JSON.parse(localStorage.getItem(index));
-			let li = document.createElement("li");
-			let ul = document.getElementById("area_list");
 
-			let name_text = document.createElement("h2");
-			let name = data.name;
-			name_text.innerText =
-				"Nome: " + (name.charAt(0).toUpperCase() + name.slice(1));
+			if (data.active) {
+				let li = document.createElement("li");
+				let ul = document.getElementById("area_list");
 
-			let adress_text = document.createElement("h3");
-			adress_text.innerText = "Endereço: " + data.adress;
+				ul.innerHTML = "";
 
-			let flavor_text = document.createElement("h3");
-			flavor_text.innerText = "Sabor de " + data.order[0].flavor;
+				let name_text = document.createElement("h2");
+				let name = data.name;
+				name_text.innerText =
+					"Nome: " + (name.charAt(0).toUpperCase() + name.slice(1));
 
-			let quantity_text = document.createElement("h3");
-			quantity_text.innerText = "Quantidade: " + data.order[0].quantity;
+				let adress_text = document.createElement("h3");
+				adress_text.innerText = "Endereço: " + data.adress;
 
-			let price_text = document.createElement("h3");
-			price_text.innerText =
-				"Preço: R$ " +
-				parseFloat(data.order[0].price).toFixed(2).replace(".", ",");
+				let flavor_text = document.createElement("h3");
+				flavor_text.innerText = "Sabor de " + data.order[0].flavor;
 
-			let button = document.createElement("button");
-      button.innerText  = 'Remover'
+				let quantity_text = document.createElement("h3");
+				quantity_text.innerText = "Quantidade: " + data.order[0].quantity;
 
-			li.appendChild(name_text);
-			li.appendChild(adress_text);
-			li.appendChild(flavor_text);
-			li.appendChild(quantity_text);
-			li.appendChild(price_text);
-			li.appendChild(button);
+				let price_text = document.createElement("h3");
+				price_text.innerText =
+					"Preço: R$ " +
+					parseFloat(data.order[0].price).toFixed(2).replace(".", ",");
 
-			ul.appendChild(li);
+				let button = document.createElement("button");
+				button.innerText = "Remover";
+
+				button.addEventListener("click", (e) => {
+					e.preventDefault();
+					remove(name);
+				});
+
+				li.appendChild(name_text);
+				li.appendChild(adress_text);
+				li.appendChild(flavor_text);
+				li.appendChild(quantity_text);
+				li.appendChild(price_text);
+				li.appendChild(button);
+
+				ul.appendChild(li);
+			}
 		}
 	});
+
 	return (
 		<main id="main_list">
 			<ul id="area_list"></ul>
