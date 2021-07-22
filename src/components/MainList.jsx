ed86a65aebb41aa444aca4fc9a5cf42ce07027df
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-22 14:23:19 -0300

    Estiliza a lista de pedidos

--- src/components/MainList.jsx
+++ src/components/MainList.jsx
@@ -1,43 +1,48 @@
 import React, { useEffect } from "react";
+import "../styles/mainList_s.css";
 
 function MainList() {
-	useEffect(() => {
-		function generateList() {
-			for (let index = 0; index < localStorage.length; index++) {
-				let data = JSON.parse(localStorage.getItem(index));
-				let li = document.createElement("li");
-				let ul = document.getElementById("list_clients");
+	useEffect(function generateList() {
+		for (let index = 0; index < localStorage.length; index++) {
+			let data = JSON.parse(localStorage.getItem(index));
+			let li = document.createElement("li");
+			let ul = document.getElementById("area_list");
 
-				let name_text = document.createElement("h2");
-				name_text.innerText = data.name;
+			let name_text = document.createElement("h2");
+			let name = data.name;
+			name_text.innerText =
+				"Nome: " + (name.charAt(0).toUpperCase() + name.slice(1));
 
-				let adress_text = document.createElement("h3");
-				adress_text.innerText = data.adress;
+			let adress_text = document.createElement("h3");
+			adress_text.innerText = "Endereço: " + data.adress;
 
-				let flavor_text = document.createElement("h3");
-				flavor_text.innerText = data.order[0].flavor;
+			let flavor_text = document.createElement("h3");
+			flavor_text.innerText = "Sabor de " + data.order[0].flavor;
 
-				let quantity_text = document.createElement("h3");
-				quantity_text.innerText = data.order[0].quantity;
+			let quantity_text = document.createElement("h3");
+			quantity_text.innerText = "Quantidade: " + data.order[0].quantity;
 
-				let price_text = document.createElement("h3");
-				price_text.innerText = data.order[0].price;
+			let price_text = document.createElement("h3");
+			price_text.innerText =
+				"Preço: R$ " +
+				parseFloat(data.order[0].price).toFixed(2).replace(".", ",");
 
-				let button = document.createElement("button");
+			let button = document.createElement("button");
+      button.innerText  = 'Remover'
 
-				li.appendChild(name_text);
-				li.appendChild(adress_text);
-				li.appendChild(quantity_text);
-				li.appendChild(price_text);
-				li.appendChild(button);
+			li.appendChild(name_text);
+			li.appendChild(adress_text);
+			li.appendChild(flavor_text);
+			li.appendChild(quantity_text);
+			li.appendChild(price_text);
+			li.appendChild(button);
 
-				ul.appendChild(li);
-			}
+			ul.appendChild(li);
 		}
 	});
 	return (
-		<main className="main_list">
-			<ul id="list_clients"></ul>
+		<main id="main_list">
+			<ul id="area_list"></ul>
 		</main>
 	);
 }
