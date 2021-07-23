15b9dd4093b469f2b8ca18116e1c674154485578
Fabrício Brazil <81660822+Braz99@users.noreply.github.com>
2021-07-23 11:16:19 -0300

    Ajusta a lógica da página de Delete

--- src/components/MainDelete.jsx
+++ src/components/MainDelete.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { handleDelete } from "../logic_app/delete_all";
 import "../styles/mainDelete_s.css";
 
 function MainDelete() {
@@ -10,7 +11,7 @@ function MainDelete() {
 				Clique no botão a seguir para apagar todos os cadastros de clientes.
 			</p>
 
-			<button id="button_delete" onClick={() => localStorage.clear()}>
+			<button id="button_delete" onClick={handleDelete}>
 				Apagar Cadastros
 			</button>
 		</main>
