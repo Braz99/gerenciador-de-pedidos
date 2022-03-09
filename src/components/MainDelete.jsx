d1028427c90431a1bf4102ad4a7c6de54822dfb9
braz99 <fabriciobrazil99@gmail.com>
2022-03-09 12:25:43 -0300

    Refatora a lógica de apagar toda a lista de dados dos clientes

--- src/components/MainDelete.jsx
+++ src/components/MainDelete.jsx
@@ -1,19 +1,38 @@
-import React from "react";
-import { handleDelete } from "../logic_app/delete_all";
+import { toast } from "react-toastify";
+import { useClients } from "../provider/ClientsProvider";
 import "../styles/mainDelete_s.css";
 
-function MainDelete() {
-	return (
-		<main id="main_delete">
-			<h1>Apagar pedidos</h1>
+export default function MainDelete() {
+  let [listClients, setListClients] = useClients();
 
-			<p>Clique no botão a seguir para apagar todos os pedidos dos clientes.</p>
+  function handleDelete() {
+    if (listClients.length > 0) {
+      localStorage.clear();
+      setListClients([]);
 
-			<button id="button_delete" onClick={handleDelete}>
-				Apagar Pedidos
-			</button>
-		</main>
-	);
+      toast.error("Pedidos apagados!", {
+        autoClose: 2500,
+        position: "top-center",
+        id: 20,
+      });
+    } else {
+      toast.warning("Não há pedidos a serem apagados!", {
+        autoClose: 2500,
+        position: "top-center",
+        id: 21,
+      });
+    }
+  }
+
+  return (
+    <main id="main_delete">
+      <h1>Apagar pedidos</h1>
+
+      <p>Clique no botão a seguir para apagar todos os pedidos dos clientes.</p>
+
+      <button id="button_delete" onClick={handleDelete}>
+        Apagar Pedidos
+      </button>
+    </main>
+  );
 }
-
-export default MainDelete;
