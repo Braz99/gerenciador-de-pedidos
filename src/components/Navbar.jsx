7b5cc4e771ea8dcbf3d3d39721bebc6c537a17cd
braz99 <fabriciobrazil99@gmail.com>
2022-03-09 14:53:26 -0300

    Ajusta o código do compoente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,29 @@
-import { useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/navbar_s.css";
 
 function Navbar() {
-	const url = useLocation();
+  const location = useLocation();
 
-	useEffect(() => {
-		if (url.pathname === "/") {
-			document.getElementById("register").classList.add("active");
-		}
+  const url = location.pathname.replace("/", "");
 
-		if (url.pathname === "/list") {
-			document.getElementById("list").classList.add("active");
-		}
-
-		if (url.pathname === "/delete") {
-			document.getElementById("delete").classList.add("active");
-		}
-	});
-
-	return (
-		<nav id="navbar">
-			<ul id="menu">
-				<li className="item_menu" id="register">
-					<Link to="/">Registrar</Link>
-				</li>
-				<li className="item_menu " id="list">
-					<Link to="/list">Listar</Link>
-				</li>
-				<li className="item_menu " id="delete">
-					<Link to="/delete">Apagar registros</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav id="navbar">
+      <ul id="menu">
+        <li className={`item_menu ${url === "" ? "active" : ""}`} id="register">
+          <Link to="/">Registrar</Link>
+        </li>
+        <li className={`item_menu ${url === "list" ? "active" : ""}`} id="list">
+          <Link to="/list">Listar</Link>
+        </li>
+        <li
+          className={`item_menu ${url === "delete" ? "active" : ""}`}
+          id="delete"
+        >
+          <Link to="/delete">Apagar registros</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
