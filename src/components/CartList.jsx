e4fa5c0c2d528abdad79a3be4f1646a645ac0122
braz99 <fabriciobrazil99@gmail.com>
2022-03-27 12:00:38 -0300

    Torna o componente cart responsível

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -3,23 +3,25 @@ import OrderInfo from "./OrderInfo";
 export default function CartList({ orders, setOrders }) {
   if (orders.list.length === 0) {
     return (
-      <div className="orders-list-cart empty">
+      <div className="orders-list-cart-empty">
         <div className="orders-list-cart-message">Carrinho vazio!</div>
       </div>
     );
   }
 
   return (
-    <div className="orders-list-cart">
-      {orders.list.map((order, i) => (
-        <OrderInfo
-          className="order-info-cart"
-          key={i}
-          orderState={{ orders, setOrders }}
-        >
-          {order}
-        </OrderInfo>
-      ))}
+    <div>
+      <div className="orders-list-cart">
+        {orders.list.map((order, i) => (
+          <OrderInfo
+            className="order-info-cart"
+            key={i}
+            orderState={{ orders, setOrders }}
+          >
+            {order}
+          </OrderInfo>
+        ))}
+      </div>
     </div>
   );
 }
